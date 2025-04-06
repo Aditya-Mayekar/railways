8c69b571d8dc0c15a38012d38240f46ecadc8dc0
Aditya Vivek Mayekar <145744562+Aditya-Mayekar@users.noreply.github.com>
2025-04-07 01:20:13 +0530

    Delete train-service directory

--- train-service/src/main/java/com/railway/train_service/service/ITrainService.java
+++
@@ -1,9 +0,0 @@
-package com.railway.train_service.service;
-
-import com.railway.train_service.dto.ResponseStructureDTO;
-import com.railway.train_service.dto.TrainDTO;
-import com.railway.train_service.dto.TrainRequestDTO;
-
-public interface ITrainService {
-    public ResponseStructureDTO<TrainDTO> addTrain(TrainRequestDTO trainRequestDTO);
-}
